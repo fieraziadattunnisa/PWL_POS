5f7c19df246052364d45c4ea772b45cbceecd075
Fiera Ziadattun Nisa <fieraziadattunnisa@gmail.com>
2025-03-04 08:33:45 +0700

    J4_Praktikum 2.3_Langkah 3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,8 +11,7 @@ class UserController extends Controller
     public function index()
     {
         
-        $user = UserModel::where('level_id', 2)->count();
-        dd($user);
-        return view('user', ['data' => $user]);
+        $userCount = UserModel::where('level_id', 2)->count();
+        return view('user', ['data' => $userCount]);
     }
 }
