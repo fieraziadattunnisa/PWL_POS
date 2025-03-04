2afb03e3bff3e3cc825cb5e520ad78df2770f5f3
Fiera Ziadattun Nisa <fieraziadattunnisa@gmail.com>
2025-03-04 09:50:27 +0700

    perubahan

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
     public function index()
     {
         
-        $user = UserModel::firstOrCreate(
+        $user = UserModel::firstOrNew(
             [
                 'username' => 'manager33',
                 'nama' => 'Manager Tiga Tiga',
@@ -19,23 +19,7 @@ class UserController extends Controller
                 'level_id' => 2
             ]
         );
-        $user->username = 'manager56';
-
-        $user->isDirty(); // true
-        $user->isDirty('username'); // true
-        $user->isDirty('nama'); // false
-        $user->isDirty(['nama', 'username']); // true
-
-        $user->isClean(); // false
-        $user->isClean('username'); // false
-        $user->isClean('nama'); // true
-        $user->isClean(['nama', 'username']); // false
-
         $user->save();
-
-        $user->isDirty(); // false
-        $user->isClean(); // true
-
-        dd($user->isDirty());
+        return view('user', ['data' => $user]);
     }
 }
