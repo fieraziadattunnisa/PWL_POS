4c452da4a9030052140eb8532415318f80efc102
Fiera Ziadattun Nisa <fieraziadattunnisa@gmail.com>
2025-03-04 08:50:43 +0700

    J4_Praktikum 2.4_Langkah 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
     public function index()
     {
         
-        $user = UserModel::firstOrCreate(
+        $user = UserModel::firstOrNew(
             [
                 'username' => 'manager',
                 'nama' => 'Manager',
