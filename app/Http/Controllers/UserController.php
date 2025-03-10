7434262a1aa3bf0e1a67d51e1d10f4a9eca72b99
Fiera Ziadattun Nisa <fieraziadattunnisa@gmail.com>
2025-03-10 10:53:10 +0700

    J5_Praktikum 3_Langkah 9

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -48,4 +48,28 @@ class UserController extends Controller
             ->rawColumns(['aksi']) //menandakan kolom tersebut adalah html
             ->make(true);
     }
+
+    // Menampilkan halaman form tambah user
+    public function create()
+    {
+        $breadcrumb = (object) [
+            'title' => 'Tambah User',
+            'list' => ['Home', 'User', 'Tambah']
+        ];
+
+        $page = (object) [
+            'title' => 'Tambah user baru'
+        ];
+
+        $level = LevelModel::all(); // ambil data level untuk ditampilkan di form
+        $activeMenu = 'user'; // set menu yang sedang aktif
+
+        return view('user.create', [
+            'breadcrumb' => $breadcrumb,
+            'page' => $page,
+            'level' => $level,
+            'activeMenu' => $activeMenu
+        ]);
+    }
+
 }
