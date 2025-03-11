70d4a97154be1fcc9d54ca9cf11c9f2ea069eae1
Fiera Ziadattun Nisa <fieraziadattunnisa@gmail.com>
2025-03-11 09:15:30 +0700

    J5_Praktikum 3_Langkah 22

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -157,4 +157,23 @@ class UserController extends Controller
 
         return redirect('/user')->with('success', 'Data user berhasil diubah');
     }
+
+    //Menghapus data user
+    public function destroy(String $id)
+    {
+        $check = UserModel::find($id);
+        if (!$check) {  
+            // untuk mengecek apakah data user dengan id yang dimaksud ada atau tidak
+            return redirect('/user')->with('error', 'Data user tidak ditemukan');
+        }
+        
+        try {
+            UserModel::destroy($id); // Hapus data user
+        
+            return redirect('/user')->with('success', 'Data user berhasil dihapus');
+        } catch (\Illuminate\Database\QueryException $e) {
+            // Jika terjadi error ketika menghapus data, redirect kembali ke halaman dengan membawa pesan error
+            return redirect('/user')->with('error', 'Data user gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
+        }        
+    }
 }
