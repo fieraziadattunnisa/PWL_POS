6ce3608ac7ad47c36dc7924458f63fd1df4a4e5c
Fiera Ziadattun Nisa <fieraziadattunnisa@gmail.com>
2025-03-12 18:07:02 +0700

    J5_Tugas_m_supplier

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -36,7 +36,13 @@ role="menu" data-accordion="false">
           <i class="nav-icon far fa-user"></i> 
           <p>Data User</p> 
         </a> 
-      </li> 
+      </li>
+      <li class="nav-item">
+        <a href="{{ url('/supplier')}}" class="nav-link {{ ($activeMenu == 'supplier') ? 'active' : '' }}">
+          <i class="nav-icon fas fa-truck"></i>
+          <p>Data Supplier</p>
+        </a>
+      </li>
       <li class="nav-header">Data Barang</li> 
       <li class="nav-item"> 
         <a href="{{ url('/kategori') }}" class="nav-link {{ ($activeMenu == 
