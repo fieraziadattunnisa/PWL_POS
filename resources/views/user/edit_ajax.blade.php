99747e7edfb9b93e751805d3fd2652b22741ff9c
Fiera Ziadattun Nisa <fieraziadattunnisa@gmail.com>
2025-03-18 08:47:38 +0700

    J6_P3_Perbaikan Fix

--- resources/views/user/edit_ajax.blade.php
+++ resources/views/user/edit_ajax.blade.php
@@ -3,7 +3,7 @@
         <div class="modal-content"> 
             <div class="modal-header"> 
                 <h5 class="modal-title" id="exampleModalLabel">Kesalahan</h5> 
-                <button type="button" class="close" data-dismiss="modal" arialabel="Close"><span aria-hidden="true">&times;</span></button> 
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button> 
             </div> 
             <div class="modal-body"> 
                 <div class="alert alert-danger"> 
@@ -21,7 +21,7 @@
         <div class="modal-content"> 
             <div class="modal-header"> 
                 <h5 class="modal-title" id="exampleModalLabel">Edit Data User</h5> 
-                <button type="button" class="close" data-dismiss="modal" arialabel="Close"><span aria-hidden="true">&times;</span></button> 
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button> 
             </div> 
             <div class="modal-body"> 
                 <div class="form-group"> 
