67a535292bcdfddfe5a6d06bad3ee0af581057e8
Fiera Ziadattun Nisa <fieraziadattunnisa@gmail.com>
2025-03-19 15:28:40 +0700

    J6_Tugas_m_level

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -2,30 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use App\Models\LevelModel;
-use Monolog\Level;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Yajra\DataTables\Facades\DataTables;
-
+use Illuminate\Support\Facades\Validator;
+use Monolog\Level;
 
 class LevelController extends Controller
 {
+    // Menampilkan halaman awal level
     public function index()
     {
-        //DB::insert('insert into m_level(level_kode, level_nama, created_at) values(?, ?, ?)', ['CUS', 'Pelanggan', now()]);
-       // return 'Insert data baru berhasil';
-
-       //$row = DB::update('update m_level set level_nama = ? where level_kode = ?', ['Customer', 'CUS']);
-        //return 'Update data berhasil. Jumlah data yang diupdate: ' . $row . ' baris';
-
-        //$row = DB::delete('delete from m_level where level_kode = ?', ['CUS']);
-        //return 'Delete data berhasil. Jumlah data yang dihapus: ' . $row . ' baris';
-
-        //$data = DB::select('select * from m_level');
-        //return view('level', ['data' => $data]);
         $breadcrumb = (object) [
-            'title' => 'Daftar Level User',
+            'title' => 'Daftar Level',
             'list' => ['Home', 'Level']
         ];
 
@@ -37,45 +27,41 @@ class LevelController extends Controller
 
         $level = LevelModel::all(); //ambil data level untuk filter level
 
-        return view('level.index', [
-            'breadcrumb' => $breadcrumb,
-            'page' => $page,
-            'level' => $level,
-            'activeMenu' => $activeMenu
-        ]);
+        return view('level.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
     }
 
-    //Ambil data user dalam bentuk json untuk datatables
+    //Ambil data level dalam bentuk json untuk datatables
     public function list(Request $request)
     {
-        $level = LevelModel::select('level_id', 'level_kode', 'level_nama');
+        $levels = LevelModel::select('level_id', 'level_kode', 'level_nama');
 
-        // Filter data user berdasarkan level_id
         if ($request->level_id) {
-            $level->where('level_id', $request->level_id);
+            $levels->where('level_id', $request->level_id);
         }
-        
-        return DataTables::of($level)
-        //menambahkan kolom index
-        ->addIndexColumn()
-        ->addColumn('aksi', function ($level) {
-            $btn = '<a href ="'.url('/level/' . $level->level_id).'" class="btn btn-info btn-sm">Detail</a> ';
-            $btn .= '<a href ="'.url('/level/' . $level->level_id . '/edit').'" class="btn btn-warning btn-sm">Edit</a> ';
-            $btn .= '<form class="d-inline-block" method="POST" action="'.url('/level/' .$level->level_id).'">'
-            .csrf_field() . method_field('DELETE') .
-            '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\');
-            ">Hapus</button></form>';
-            return $btn;
-        })
-        ->rawColumns(['aksi'])
-        ->make(true);
+
+        return DataTables::of($levels)
+            ->addIndexColumn()
+            ->addColumn('aksi', function ($level) {
+                /*$btn = '<a href="' . url('/level/' . $level->level_id) . '" class="btn btn-info btn-sm">Detail</a> ';
+                $btn .= '<a href="' . url('/level/' . $level->level_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a> ';
+                $btn .= '<form class="d-inline-block" method="POST" action="' . url('/level/' . $level->level_id) . '">'
+                    . csrf_field() . method_field('DELETE') .
+                    '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\');">Hapus</button></form>';*/
+                $btn  = '<button onclick="modalAction(\'' . url('/level/' . $level->level_id . '/show_ajax') . '\')" class="btn btn-info btn-sm">Detail</button> ';
+                $btn .= '<button onclick="modalAction(\'' . url('/level/' . $level->level_id . '/edit_ajax') . '\')" class="btn btn-warning btn-sm">Edit</button> ';
+                $btn .= '<button onclick="modalAction(\'' . url('/level/' . $level->level_id . '/delete_ajax') . '\')"  class="btn btn-danger btn-sm">Hapus</button> ';
+                return $btn;
+            })
+            ->rawColumns(['aksi'])
+            ->make(true);
     }
 
-    // Menampilkan halaman form tambah user
-    public function create()
+
+    //Menampilkan halaman form tambah level
+    /*public function create()
     {
         $breadcrumb = (object) [
-            'title' => 'Tambah Level User',
+            'title' => 'Tambah Level',
             'list' => ['Home', 'Level', 'Tambah']
         ];
 
@@ -83,21 +69,18 @@ class LevelController extends Controller
             'title' => 'Tambah level baru'
         ];
 
-        $activeMenu = 'level'; // set menu yang sedang aktif
+        $level = LevelModel::all();
+        $activeMenu = 'level';
 
-        return view('level.create', [
-            'breadcrumb' => $breadcrumb,
-            'page' => $page,
-            'activeMenu' => $activeMenu
-        ]);
+        return view('level.create', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
     }
 
-    // Menyimpan data user baru
+    // Menyimpan data level baru
     public function store(Request $request)
     {
         $request->validate([
-            'level_kode' => 'required|string|max:3',
-            'level_nama' => 'required|string',
+            'level_kode' => 'required|string|max:10|unique:m_level,level_kode',
+            'level_nama' => 'required|string|max:100',
         ]);
 
         LevelModel::create([
@@ -105,52 +88,179 @@ class LevelController extends Controller
             'level_nama' => $request->level_nama,
         ]);
 
-        return redirect('/level')->with('success', 'Data level user berhasil disimpan');
+        return redirect('/level')->with('success', 'Data level berhasil disimpan');
+    }*/
+
+    // Menambah data baru dengan ajax
+    public function create_ajax()
+    {
+        $level = LevelModel::select('level_kode', 'level_nama')->get();
+
+        return view('level.create_ajax')
+            ->with('level', $level);
     }
 
-    // Menampilkan detail user
-    public function show(string $id)
+    //Simpan data melalui ajax
+    public function store_ajax(Request $request)
+    {
+        //cek apakah request berupa ajax
+        if ($request->ajax() || $request->wantsJson()) {
+
+            $rules = [
+                'level_kode' => 'required|string|max:10|unique:m_level,level_kode',
+                'level_nama' => 'required|string|max:100',
+            ];
+
+            // use Illuminate\Support\Facades\Validator;
+            $validator = Validator::make($request->all(), $rules);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => false, //response status, false: error/gagal, true:berhasil
+                    'message' => 'Validasi Gagal',
+                    'msgField' => $validator->errors(), //pesan error validasi
+                ]);
+            }
+
+            LevelModel::create($request->all());
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Data level berhasil disimpan'
+            ]);
+        }
+        redirect('/');
+    }
+
+    //Menampilkan halaman form edit level ajax
+    public function edit_ajax(string $id)
+    {
+        $level = LevelModel::find($id);
+
+        return view('level.edit_ajax', ['level' => $level]);
+    }
+
+    //Mengakomodir request update data level melalui ajax
+    public function update_ajax(Request $request, $id)
+    {
+        // cek apakah request dari ajax
+        if ($request->ajax() || $request->wantsJson()) {
+            $rules = [
+                'level_kode' => 'required|min:3|unique:m_level,level_kode,' . $id . ',level_id',
+                'level_nama' => 'required|max:100'
+            ];
+
+            // use Illuminate\Support\Facades\Validator;
+            $validator = Validator::make($request->all(), $rules);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => false, //respon json, true:berhasil, false:gagal
+                    'message' => 'Validasi gagal.',
+                    'msgField' => $validator->errors() // menunjukkan field mana yang error
+                ]);
+            }
+
+            $check = LevelModel::find($id);
+            if ($check) {
+                if (!$request->filled('level_kode')) { // jika level kode tidak diisi, maka hapus dari request
+                    $request->request->remove('level_kode');
+                }
+                $check->update($request->all());
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Data berhasil diupdate'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Data tidak ditemukan'
+                ]);
+            }
+        }
+        return redirect('/');
+    }
+
+     //menampilkan detail data level dengan ajax
+     public function show_ajax($id)
+     {
+         $level = LevelModel::find($id);
+         return view('level.show_ajax', ['level' => $level]);
+     }
+
+    //Confirm ajax
+    public function confirm_ajax(string $id)
+    {
+        $level = LevelModel::find($id);
+
+        return view('level.confirm_ajax', ['level' => $level]);
+    }
+
+    // Delete ajax
+    public function delete_ajax(Request $request, $id)
+    {
+        if ($request->ajax() || $request->wantsJson()) {
+            $level = LevelModel::find($id);
+            if ($level) {
+                $level->delete();
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Data berhasil dihapus'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Data tidak ditemukan'
+                ]);
+            }
+        }
+        return redirect('/');
+    }
+
+
+    // Menampilkan detail level
+    /*public function show(string $id)
     {
         $level = LevelModel::find($id);
 
         $breadcrumb = (object) [
-            'title' => 'Detail Level User',
+            'title' => 'Detail Level',
             'list' => ['Home', 'Level', 'Detail']
         ];
 
         $page = (object) [
-            'title' => 'Detail level user'
+            'title' => 'Detail level'
         ];
 
-        $activeMenu = 'level'; // set menu yang sedang aktif
+        $activeMenu = 'level';
 
         return view('level.show', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
     }
 
-    // Menampilkan halaman form edit user
+    //Menampilkan halaman form edit level
     public function edit(string $id)
     {
         $level = LevelModel::find($id);
 
         $breadcrumb = (object) [
-            'title' => 'Edit Level User',
-            'list'  => ['Home', 'Level', 'Edit']
+            'title' => 'Edit level',
+            'list' => ['Home', 'Level', 'Edit']
         ];
 
         $page = (object) [
-            'title' => 'Edit level user'
+            'title' => 'Edit level'
         ];
 
-        $activeMenu = 'level'; // set menu yang sedang aktif
+        $activeMenu = 'level'; //set menu yang sedang aktif
 
         return view('level.edit', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
     }
 
-    // Menyimpan perubahan data user
+    //Menyimpan perubahan data level
     public function update(Request $request, string $id)
     {
         $request->validate([
-            'level_kode' => 'required|string|max:10|unique:m_level,level_kode,'.$id.',level_id',
+            'level_kode' => 'required|string|max:10|unique:m_level,level_kode,' . $id . ',level_id',
             'level_nama' => 'required|string|max:100', //nama harus diisi, max 100 karakter
         ]);
 
@@ -162,22 +272,21 @@ class LevelController extends Controller
         return redirect('/level')->with('success', 'Data level berhasil diubah');
     }
 
-    //Menghapus data user
-    public function destroy(String $id)
+    //Menghapus data level
+    public function destroy(string $id)
     {
         $check = LevelModel::find($id);
         if (!$check) {
-            return redirect('/level')->with('error', 'Data user tidak ditemukan');
+            return redirect('/level')->with('error', 'Data level berhasil dihapus');
         }
         try {
+            LevelModel::destroy($id); // Hapus data level
 
-            LevelModel::destroy($id); // Hapus data user
-
-            return redirect('/level')->with('success', 'Data user berhasil dihapus');
+            return redirect('/level')->with('success', 'Data level berhasil dihapus');
         } catch (\Illuminate\Database\QueryException $e) {
 
             // Jika terjadi error ketika menghapus data, redirect kembali ke halaman dengan membawa pesan error
-            return redirect('/level')->with('error', 'Data user gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
-        }        
-    }
+            return redirect('/level')->with('error', 'Data level gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
+        }
+    }*/
 }
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
-use App\Http\Controllers\BarangController;
-use App\Http\Controllers\SupplierController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
-use Monolog\Level;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,67 +20,100 @@ use Monolog\Level;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/level', [LevelController::class, 'index']);
+
+Route::get('/kategori', [KategoriController::class, 'index']);
+
+Route::get('/user', [UserController::class, 'index']);
+
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+
+Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan'])->name('user.ubah_simpan');
+
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
 Route::get('/', [WelcomeController::class, 'index']);
 
 Route::group(['prefix' => 'user'], function () {
-    Route::get('/', [UserController::class, 'index']);      // menampilkan halaman awal user
-    Route::post('/list', [UserController::class, 'list']);  // menampilkan data user dalam bentuk json untuk datatables
-    Route::get('/create', [UserController::class, 'create']); // menampilkan halaman form tambah user
-    Route::post('/', [UserController::class, 'store']);     // menyimpan data user baru
-    Route::get('/create_ajax', [UserController::class, 'create_ajax']); // Menampilkan halaman form tambah user Ajax
-    Route::post('/ajax', [UserController::class, 'store_ajax']); // Menyimpan data user baru Ajax
-    Route::get('/{id}', [UserController::class, 'show']);   // menampilkan detail user
+    Route::get('/', [UserController::class, 'index']); //menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']); //menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']); //menampilkan halaman form tambah data user
+    Route::post('/', [UserController::class, 'store']); // menyimpan data user baru
+    Route::get('/create_ajax', [UserController:: class, 'create_ajax']); //Menampilkan halaman form tambah user Ajax
+    Route::post('/ajax', [UserController::class, 'store_ajax']); //Menyimpan data user baru Ajax
+    Route::get('/{id}', [UserController::class, 'show']); // menampilkan detail user
     Route::get('/{id}/edit', [UserController::class, 'edit']); // menampilkan halaman form edit user
-    Route::put('/{id}', [UserController::class, 'update']); // menyimpan perubahan data user
-    Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']); // Menampilkan halaman form edit user Ajax
-    Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']); // Menyimpan perubahan data user Ajax
-    Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']); // Untuk tampilkan form confirm delete user Ajax
-    Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']); // Untuk hapus data user Ajax
-    Route::get('/{id}/show_ajax', [UserController::class, 'show_ajax']); // Untuk tampilkan detail user Ajax
-    Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
-    Route::post('/user/list', [UserController::class, 'list'])->name('user.list');
+    Route::put('/{id}', [UserController::class, 'update']); //menyimpan perubahan data user
+    Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']); //Menampilkan halaman form edit user Ajax
+    Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']); //Menyimpan perubahan data user ajax
+    Route::get('/{id}/show_ajax', [UserController::class, 'show_ajax']); //Menampilkan detail data user dengan ajax
+    Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']); //Untuk tampilkan form confirm delete user Ajax
+    Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']); //Untuk hapus data user Ajax
+    Route::delete('/{id}', [UserController::class, 'destroy']); //menghapus data user
 });
 
 Route::group(['prefix' => 'level'], function () {
-    Route::get('/', [LevelController::class, 'index']);      // menampilkan halaman awal user
-    Route::post('/list', [LevelController::class, 'list']);  // menampilkan data user dalam bentuk json untuk datatables
-    Route::get('/create', [LevelController::class, 'create']); // menampilkan halaman form tambah user
-    Route::post('/', [LevelController::class, 'store']);     // menyimpan data user baru
-    Route::get('/{id}', [LevelController::class, 'show']);   // menampilkan detail user
-    Route::get('/{id}/edit', [LevelController::class, 'edit']); // menampilkan halaman form edit user
-    Route::put('/{id}', [LevelController::class, 'update']); // menyimpan perubahan data user
-    Route::delete('/{id}', [LevelController::class, 'destroy']); // menghapus data user
+    Route::get('/', [LevelController::class, 'index']); //menampilkan halaman awal level
+    Route::post('/list', [LevelController::class, 'list']); //menampilkan data level dalam bentuk json untuk datatables
+    Route::get('/create', [LevelController::class, 'create']); //menampilkan halaman form tambah data level
+    Route::post('/', [LevelController::class, 'store']); // menyimpan data level baru
+    Route::get('/create_ajax', [LevelController:: class, 'create_ajax']); //Menampilkan halaman form tambah level Ajax
+    Route::post('/ajax', [LevelController::class, 'store_ajax']); //Menyimpan data level baru Ajax
+    Route::get('/{id}', [LevelController::class, 'show']); // menampilkan detail level
+    Route::get('/{id}/edit', [LevelController::class, 'edit']); // menampilkan halaman form edit level
+    Route::put('/{id}', [LevelController::class, 'update']); //menyimpan perubahan data level
+    Route::get('/{id}/edit_ajax', [LevelController::class, 'edit_ajax']); //Menampilkan halaman form edit level Ajax
+    Route::put('/{id}/update_ajax', [LevelController::class, 'update_ajax']); //Menyimpan perubahan data level ajax
+    Route::get('/{id}/show_ajax', [LevelController::class, 'show_ajax']); //Menampilkan detail data level dengan ajax
+    Route::get('/{id}/delete_ajax', [LevelController::class, 'confirm_ajax']); //Untuk tampilkan form confirm delete level Ajax
+    Route::delete('/{id}/delete_ajax', [LevelController::class, 'delete_ajax']); //Untuk hapus data level Ajax
+    Route::delete('/{id}', [LevelController::class, 'destroy']); //menghapus data level
 });
 
 Route::group(['prefix' => 'kategori'], function () {
-    Route::get('/', [KategoriController::class, 'index']);      // menampilkan halaman awal user
-    Route::post('/list', [KategoriController::class, 'list']);  // menampilkan data user dalam bentuk json untuk datatables
-    Route::get('/create', [KategoriController::class, 'create']); // menampilkan halaman form tambah user
-    Route::post('/', [KategoriController::class, 'store']);     // menyimpan data user baru
-    Route::get('/{id}', [KategoriController::class, 'show']);   // menampilkan detail user
-    Route::get('/{id}/edit', [KategoriController::class, 'edit']); // menampilkan halaman form edit user
-    Route::put('/{id}', [KategoriController::class, 'update']); // menyimpan perubahan data user
-    Route::delete('/{id}', [KategoriController::class, 'destroy']); // menghapus data user
+    Route::get('/', [KategoriController::class, 'index']); //menampilkan halaman awal kategori
+    Route::post('/list', [KategoriController::class, 'list']); //menampilkan data kategori dalam bentuk json untuk datatables
+    Route::get('/create', [KategoriController::class, 'create']); //menampilkan halaman form tambah data kategori
+    Route::post('/', [KategoriController::class, 'store']); // menyimpan data kategori baru
+    Route::get('/create_ajax', [KategoriController:: class, 'create_ajax']); //Menampilkan halaman form tambah kategori Ajax
+    Route::post('/ajax', [KategoriController::class, 'store_ajax']); //Menyimpan data kategori baru Ajax
+    Route::get('/{id}', [KategoriController::class, 'show']); // menampilkan detail kategori
+    Route::get('/{id}/edit', [KategoriController::class, 'edit']); // menampilkan halaman form edit kategori
+    Route::put('/{id}', [KategoriController::class, 'update']); //menyimpan perubahan data kategori
+    Route::get('/{id}/edit_ajax', [KategoriController::class, 'edit_ajax']); //Menampilkan halaman form edit kategori Ajax
+    Route::put('/{id}/update_ajax', [KategoriController::class, 'update_ajax']); //Menyimpan perubahan data kategori ajax
+    Route::get('/{id}/show_ajax', [KategoriController::class, 'show_ajax']); //Menampilkan detail data kategori dengan ajax
+    Route::get('/{id}/delete_ajax', [KategoriController::class, 'confirm_ajax']); //Untuk tampilkan form confirm delete kategori Ajax
+    Route::delete('/{id}/delete_ajax', [KategoriController::class, 'delete_ajax']); //Untuk hapus data kategori Ajax
+    Route::delete('/{id}', [KategoriController::class, 'destroy']); //menghapus data kategori
 });
 
-Route::group(['prefix' => 'barang'], function(){
-    Route::get('/', [BarangController::class, 'index']); // menampilkan halaman awal barang
-    Route::post('/list', [BarangController::class, 'list']); // menampilkan data barang dalam bentuk json untuk datables
-    Route::get('/create', [BarangController::class, 'create']); // menampilkan halaman form tambah barang
-    Route::post('/', [BarangController::class, 'store']); // menyimpan data barang baru
-    Route::get('/{id}', [BarangController::class, 'show']); // menampilkan detail barang
-    Route::get('/{id}/edit', [BarangController::class, 'edit']); // menampilkan halaman form edit barang
-    Route::put('/{id}', [BarangController::class, 'update']); // menyimpan data barang yang diubah
-    Route::delete('/{id}', [BarangController::class, 'destroy']); // menghapus data barang
-});
-
-Route::group(['prefix' => 'supplier'], function(){
-    Route::get('/', [SupplierController::class, 'index']); // menampilkan halaman awal supplier
-    Route::post('/list', [SupplierController::class, 'list']); // menampilkan data supplier dalam bentuk json untuk datables
-    Route::get('/create', [SupplierController::class, 'create']); // menampilkan halaman form tambah supplier
+Route::group(['prefix' => 'supplier'], function () {
+    Route::get('/', [SupplierController::class, 'index']); //menampilkan halaman awal supplier
+    Route::post('/list', [SupplierController::class, 'list']); //menampilkan data supplier dalam bentuk json untuk datatables
+    Route::get('/create', [SupplierController::class, 'create']); //menampilkan halaman form tambah data supplier
     Route::post('/', [SupplierController::class, 'store']); // menyimpan data supplier baru
     Route::get('/{id}', [SupplierController::class, 'show']); // menampilkan detail supplier
     Route::get('/{id}/edit', [SupplierController::class, 'edit']); // menampilkan halaman form edit supplier
-    Route::put('/{id}', [SupplierController::class, 'update']); // menyimpan data supplier yang diubah
-    Route::delete('/{id}', [SupplierController::class, 'destroy']); // menghapus data supplier
+    Route::put('/{id}', [SupplierController::class, 'update']); //menyimpan perubahan data supplier
+    Route::delete('/{id}', [SupplierController::class, 'destroy']); //menghapus data supplier
+});
+
+Route::group(['prefix' => 'barang'], function () {
+    Route::get('/', [BarangController::class, 'index']); //menampilkan halaman awal barang
+    Route::post('/list', [BarangController::class, 'list']); //menampilkan data barang dalam bentuk json untuk datatables
+    Route::get('/create', [BarangController::class, 'create']); //menampilkan halaman form tambah data barang
+    Route::post('/', [BarangController::class, 'store']); // menyimpan data barang baru
+    Route::get('/{id}', [BarangController::class, 'show']); // menampilkan detail barang
+    Route::get('/{id}/edit', [BarangController::class, 'edit']); // menampilkan halaman form edit barang
+    Route::put('/{id}', [BarangController::class, 'update']); //menyimpan perubahan data barang
+    Route::delete('/{id}', [BarangController::class, 'destroy']); //menghapus data barang
 });
